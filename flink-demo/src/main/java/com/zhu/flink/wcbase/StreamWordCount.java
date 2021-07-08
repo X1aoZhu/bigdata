9de982bceee7413d3a711c76cec3d5d718aca845
zhuhaibo <zhuhaibo@asinking.com>
2021-07-08 14:38:42 +0800

    flink current wordCount

--- flink-demo/src/main/java/com/zhu/flink/wcbase/StreamWordCount.java
+++ flink-demo/src/main/java/com/zhu/flink/wcbase/StreamWordCount.java
@@ -2,6 +2,7 @@ package com.zhu.flink.wcbase;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
@@ -9,6 +10,7 @@ import org.apache.flink.util.Collector;
 
 /**
  * Flink stream
+ *
  * @Author ZhuHaiBo
  * @Create 2021/7/7 23:46
  */
@@ -16,10 +18,25 @@ public class StreamWordCount {
     public static void main(String[] args) throws Exception {
         baseWordCount();
 
-        //StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        streamCurrentWordCount(args);
+    }
 
+    /**
+     * @param args main启动参数
+     * @throws Exception
+     */
+    private static void streamCurrentWordCount(String[] args) throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 获取启动参数 --host hadoop1 --port 7000 nc -lk 7000
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int post = Integer.parseInt(parameterTool.get("port"));
 
+        DataStreamSource<String> dataStreamSource = environment.socketTextStream(host, post);
 
+        dataStreamSource.flatMap(new MyFlatMap()).keyBy(0).sum(1).print();
+
+        environment.execute();
     }
 
     private static void baseWordCount() throws Exception {
@@ -34,7 +51,7 @@ public class StreamWordCount {
     }
 
 
-    private static class MyFlatMap implements FlatMapFunction<String, Tuple2<String, Integer>>  {
+    private static class MyFlatMap implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
         @Override
         public void flatMap(String line, Collector<Tuple2<String, Integer>> collector) throws Exception {
