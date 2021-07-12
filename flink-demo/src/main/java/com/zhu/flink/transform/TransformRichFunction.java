44192866db6344f4374b01a0ce289865428c406b
zhuhaibo <zhuhaibo@asinking.com>
2021-07-12 18:46:55 +0800

    flink transform RichFunction

---
+++ flink-demo/src/main/java/com/zhu/flink/transform/TransformRichFunction.java
@@ -0,0 +1,13 @@
+package com.zhu.flink.transform;
+
+/**
+ * 富函数
+ *
+ * @author: ZhuHaiBo
+ * @date: 2021/7/12  18:41
+ */
+public class TransformRichFunction {
+    public static void main(String[] args) {
+
+    }
+}
