c9fe2657646546ac0f01bcccd83f4b297af67f08
zhuhaibo <zhuhaibo@asinking.com>
2021-07-22 18:32:56 +0800

    Flink CDC 01

---
+++ flink-exec-1.13/src/main/java/com/zhu/flink/cdc/CdcExec.java
@@ -0,0 +1,95 @@
+package com.zhu.flink.cdc;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+/**
+ * @author: ZhuHaiBo
+ * @date: 2021/7/22  18:00
+ */
+public class CdcExec {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(executionEnvironment);
+
+        tableEnvironment.executeSql("CREATE TABLE products\n" +
+                "(\n" +
+                "    id INT,\n" +
+                "    name STRING,\n" +
+                "    description STRING\n" +
+                ")\n" +
+                "WITH (\n" +
+                "    'connector' = 'mysql-cdc',\n" +
+                "    'hostname' = 'hadoop1',\n" +
+                "    'port' = '3306',\n" +
+                "    'username' = 'root',\n" +
+                "    'password' = 'root',\n" +
+                "    'database-name' = 'flink_cdc',\n" +
+                "    'table-name' = 'products'\n" +
+                ");");
+
+        tableEnvironment.executeSql("CREATE TABLE orders (\n" +
+                "  order_id INT,\n" +
+                "  order_date TIMESTAMP(0),\n" +
+                "  customer_name STRING,\n" +
+                "  price DECIMAL(10, 5),\n" +
+                "  product_id INT,\n" +
+                "  order_status BOOLEAN\n" +
+                ") WITH (\n" +
+                "  'connector' = 'mysql-cdc',\n" +
+                "  'hostname' = 'hadoop1',\n" +
+                "  'port' = '3306',\n" +
+                "  'username' = 'root',\n" +
+                "  'password' = 'root',\n" +
+                "  'database-name' = 'flink_cdc',\n" +
+                "  'table-name' = 'orders'\n" +
+                ");");
+
+        tableEnvironment.executeSql("CREATE TABLE shipments (\n" +
+                "  shipment_id INT,\n" +
+                "  order_id INT,\n" +
+                "  origin STRING,\n" +
+                "  destination STRING,\n" +
+                "  is_arrived BOOLEAN\n" +
+                ") WITH (\n" +
+                "  'connector' = 'postgres-cdc',\n" +
+                "  'hostname' = 'hadoop1',\n" +
+                "  'port' = '3306',\n" +
+                "  'username' = 'root',\n" +
+                "  'password' = 'root',\n" +
+                "  'database-name' = 'flink_cdc',\n" +
+                "  'table-name' = 'shipments'\n" +
+                ");");
+
+        tableEnvironment.executeSql("CREATE TABLE enriched_orders (\n" +
+                "  order_id INT,\n" +
+                "  order_date TIMESTAMP(0),\n" +
+                "  customer_name STRING,\n" +
+                "  price DECIMAL(10, 5),\n" +
+                "  product_id INT,\n" +
+                "  order_status BOOLEAN,\n" +
+                "  product_name STRING,\n" +
+                "  product_description STRING,\n" +
+                "  shipment_id INT,\n" +
+                "  origin STRING,\n" +
+                "  destination STRING,\n" +
+                "  is_arrived BOOLEAN,\n" +
+                "  PRIMARY KEY (order_id) NOT ENFORCED\n" +
+                ") WITH (\n" +
+                "    'connector' = 'elasticsearch-7',\n" +
+                "    'hosts' = 'hadoop2:9200',\n" +
+                "    'index' = 'enriched_orders'\n" +
+                ");");
+
+        tableEnvironment.executeSql("INSERT INTO enriched_orders\n" +
+                "SELECT o.*, p.name, p.description, s.shipment_id, s.origin, s.destination, s.is_arrived\n" +
+                "FROM orders AS o\n" +
+                "LEFT JOIN products AS p ON o.product_id = p.id\n" +
+                "LEFT JOIN shipments AS s ON o.order_id = s.order_id;");
+
+
+        executionEnvironment.execute();
+
+    }
+}
