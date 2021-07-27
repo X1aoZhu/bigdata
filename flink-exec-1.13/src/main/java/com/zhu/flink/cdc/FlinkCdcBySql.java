6cd235f5e7e95d62f237fba8851e6ae05ff36eb8
zhuhaibo <zhuhaibo@asinking.com>
2021-07-27 18:01:52 +0800

    Flink CDC 02

--- flink-exec-1.13/src/main/java/com/zhu/flink/cdc/FlinkCdcBySql.java
+++ flink-exec-1.13/src/main/java/com/zhu/flink/cdc/FlinkCdcBySql.java
@@ -1,33 +1,36 @@
 package com.zhu.flink.cdc;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
 
 /**
  * @author: ZhuHaiBo
  * @date: 2021/7/22  18:00
  */
-public class CdcExec {
-    public static void main(String[] args) throws Exception{
-        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+public class FlinkCdcBySql {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(executionEnvironment);
+        EnvironmentSettings environmentSettings = EnvironmentSettings.newInstance().inStreamingMode().useBlinkPlanner().build();
+        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(environment, environmentSettings);
 
-        tableEnvironment.executeSql("CREATE TABLE products\n" +
-                "(\n" +
-                "    id INT,\n" +
-                "    name STRING,\n" +
-                "    description STRING\n" +
-                ")\n" +
-                "WITH (\n" +
-                "    'connector' = 'mysql-cdc',\n" +
-                "    'hostname' = 'hadoop1',\n" +
-                "    'port' = '3306',\n" +
-                "    'username' = 'root',\n" +
-                "    'password' = 'root',\n" +
-                "    'database-name' = 'flink_cdc',\n" +
-                "    'table-name' = 'products'\n" +
-                ");");
+
+        tableEnvironment.executeSql("CREATE TABLE products (\n" +
+                "  id INT,\n" +
+                "  name STRING,\n" +
+                "  description STRING\n" +
+                ") WITH (\n" +
+                "  'connector' = 'mysql-cdc',\n" +
+                "  'hostname' = 'hadoop1',\n" +
+                "  'port' = '3306',\n" +
+                "  'username' = 'root',\n" +
+                "  'password' = 'root',\n" +
+                "  'database-name' = 'flink_cdc',\n" +
+                "  'table-name' = 'products'\n" +
+                ")");
 
         tableEnvironment.executeSql("CREATE TABLE orders (\n" +
                 "  order_id INT,\n" +
@@ -44,7 +47,7 @@ public class CdcExec {
                 "  'password' = 'root',\n" +
                 "  'database-name' = 'flink_cdc',\n" +
                 "  'table-name' = 'orders'\n" +
-                ");");
+                ")");
 
         tableEnvironment.executeSql("CREATE TABLE shipments (\n" +
                 "  shipment_id INT,\n" +
@@ -53,14 +56,14 @@ public class CdcExec {
                 "  destination STRING,\n" +
                 "  is_arrived BOOLEAN\n" +
                 ") WITH (\n" +
-                "  'connector' = 'postgres-cdc',\n" +
+                "  'connector' = 'mysql-cdc',\n" +
                 "  'hostname' = 'hadoop1',\n" +
                 "  'port' = '3306',\n" +
                 "  'username' = 'root',\n" +
                 "  'password' = 'root',\n" +
                 "  'database-name' = 'flink_cdc',\n" +
                 "  'table-name' = 'shipments'\n" +
-                ");");
+                ")");
 
         tableEnvironment.executeSql("CREATE TABLE enriched_orders (\n" +
                 "  order_id INT,\n" +
@@ -78,18 +81,19 @@ public class CdcExec {
                 "  PRIMARY KEY (order_id) NOT ENFORCED\n" +
                 ") WITH (\n" +
                 "    'connector' = 'elasticsearch-7',\n" +
-                "    'hosts' = 'hadoop2:9200',\n" +
+                "    'hosts' = 'http://hadoop2:9200',\n" +
                 "    'index' = 'enriched_orders'\n" +
-                ");");
+                ")");
 
-        tableEnvironment.executeSql("INSERT INTO enriched_orders\n" +
-                "SELECT o.*, p.name, p.description, s.shipment_id, s.origin, s.destination, s.is_arrived\n" +
-                "FROM orders AS o\n" +
-                "LEFT JOIN products AS p ON o.product_id = p.id\n" +
-                "LEFT JOIN shipments AS s ON o.order_id = s.order_id;");
+        Table productTable = tableEnvironment.sqlQuery("select * from products");
+        tableEnvironment.toRetractStream(productTable, Row.class).print("Product");
 
+        Table orderTable = tableEnvironment.sqlQuery("select * from orders");
+        tableEnvironment.toRetractStream(orderTable, Row.class).print("Order");
 
-        executionEnvironment.execute();
+        Table shipmentTable = tableEnvironment.sqlQuery("select * from shipments");
+        tableEnvironment.toRetractStream(shipmentTable, Row.class).print("Shipments");
 
+        environment.execute();
     }
 }
