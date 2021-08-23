c1cc601867adb0b37f4d65f0acefebf4042dc8c3
zhuhaibo <zhuhaibo@asinking.com>
2021-08-23 17:00:33 +0800

    kafka-exec init

---
+++ kafka-exec/src/main/java/com/zhu/kafka/KafkaApplication.java
@@ -0,0 +1,15 @@
+package com.zhu.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ZhuHaiBo
+ * @date: 2021/8/23  16:54
+ */
+@SpringBootApplication
+public class KafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+}
