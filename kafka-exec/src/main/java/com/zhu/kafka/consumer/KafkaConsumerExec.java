1fd184aac93e55959a327aba3fdc8481353ec2b0
zhu <1838034329@qq.com>
2021-08-26 22:51:34 +0800

    kafka consumer

--- kafka-exec/src/main/java/com/zhu/kafka/consumer/KafkaConsumerExec.java
+++ kafka-exec/src/main/java/com/zhu/kafka/consumer/KafkaConsumerExec.java
@@ -1,11 +1,19 @@
 package com.zhu.kafka.consumer;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.header.Headers;
+import org.slf4j.LoggerFactory;
 
+
+import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 /**
  * @Author ZhuHaiBo
@@ -16,15 +24,29 @@ public class KafkaConsumerExec {
 
     private final static String TOPIC = "test";
 
+    static {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        Logger root = loggerContext.getLogger("root");
+        root.setLevel(Level.INFO);
+    }
+
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.put("bootstrap-servers", "192.168.240.152:9092");
+        properties.put("bootstrap.servers", "192.168.240.152:9092");
+        properties.put("group.id", "a");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("auto.offset.reset", "earliest");
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
         kafkaConsumer.subscribe(Collections.singleton(TOPIC));
 
         while (true) {
-            //kafkaConsumer.poll()
+            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(5L));
+            for (ConsumerRecord<String, String> record : consumerRecords) {
+                Headers headers = record.headers();
+                log.info("key:[{}],value:[{}],partition：[{}],header:[{}]", record.key(), record.value(), record.partition(), headers.toArray());
+            }
         }
 
     }
